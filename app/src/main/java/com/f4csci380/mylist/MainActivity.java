fed87a9dc160c707a005a44480e99b0978b0a217
Karan Vadhan <karanvadhan789@gmail.com>
2019-12-06 19:04:26 -0500

    recycler view is better then list view
    and thats a fact

--- app/src/main/java/com/f4csci380/mylist/MainActivity.java
+++ app/src/main/java/com/f4csci380/mylist/MainActivity.java
@@ -1,13 +1,24 @@
 package com.f4csci380.mylist;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    RecyclerView memoList;
+    Button addMemo;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        memoList = findViewById(R.id.memoList);
+        addMemo = findViewById(R.id.addMemo);
     }
 }
---
+++ app/src/main/java/com/f4csci380/mylist/Memo.java
@@ -0,0 +1,6 @@
+package com.f4csci380.mylist;
+
+public class Memo
+{
+
+}
