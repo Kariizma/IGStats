15d2fbe691f447a99a1cb2697216a69881199be8
Karan Vadhan <karanvadhan789@gmail.com>
2019-12-09 14:24:07 -0500

    can add and delete memos
    feelsgoodman

--- app/src/main/java/com/f4csci380/mylist/ComposeActivity.java
+++ app/src/main/java/com/f4csci380/mylist/ComposeActivity.java
@@ -2,15 +2,24 @@ package com.f4csci380.mylist;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+
+import static com.f4csci380.mylist.MainActivity.memos;
 
 public class ComposeActivity extends AppCompatActivity {
 
     EditText title;
     EditText body;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,5 +29,30 @@ public class ComposeActivity extends AppCompatActivity {
 
         title = findViewById(R.id.title);
         body = findViewById(R.id.body);
+        saveButton = findViewById(R.id.saveBtn);
+
+        Intent intent = getIntent();
+        final int memoID = intent.getIntExtra("MemoID",-1);
+
+        if(memoID != -1)
+        {
+            Memo memo = memos.get(memoID);
+            String getTitle = memo.getTitle();
+            String getBody = memo.getBody();
+            title.setText(getTitle);
+            body.setText(getBody);
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                memos.get(memoID).setTitle(title.getText().toString());
+                memos.get(memoID).setBody(body.getText().toString());
+                MainActivity.memoAdapter.notifyDataSetChanged();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/f4csci380/mylist/MainActivity.java
+++ app/src/main/java/com/f4csci380/mylist/MainActivity.java
@@ -1,9 +1,11 @@
 package com.f4csci380.mylist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,9 +17,9 @@ public class MainActivity extends AppCompatActivity
 {
     public static final String TAG = "MainActivity";
 
-    ArrayList<Memo> memos;
+    static ArrayList<Memo> memos;
     RecyclerView memoList;
-    MemoAdapter memoAdapter;
+    static MemoAdapter memoAdapter;
     Button addMemo;
 
     @Override
@@ -40,6 +42,15 @@ public class MainActivity extends AppCompatActivity
         memoList.setLayoutManager(new LinearLayoutManager(this));
         memoList.setAdapter(memoAdapter);
 
+        addMemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                memos.add(new Memo("New Memo", "New Memo"));
+                memoAdapter.notifyDataSetChanged();
+
+            }
+        });
+
         memoAdapter.setOnItemClickListner(new MemoAdapter.ClickListener() {
             @Override
             public void onItemClick(int position, View v)
@@ -50,8 +61,10 @@ public class MainActivity extends AppCompatActivity
             }
 
             @Override
-            public void onItemLongClick(int position, View v) {
-
+            public void onItemLongClick(int position, View v)
+            {
+                memos.remove(position);
+                memoAdapter.notifyDataSetChanged();
             }
         });
 
